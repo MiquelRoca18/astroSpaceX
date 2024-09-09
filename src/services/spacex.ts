7019aee13638fc9dfef078bc4db443fd0394bdbe
MiquelRoca18 <miquel1818@gmail.com>
2024-09-09 16:39:29 +0200

    first commit

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -1,30 +1,30 @@
+// spacex.ts
 import {type Doc, type APISpaceXResponse} from '../types/api'
 
-export const getLaunchBy = async ({id}: { id: string}) =>{
-    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
-
-    const launch = (await res.json()) as Doc
-    return launch
+export const getLaunchBy = async ({ id }: { id: string }) => {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+    const launch = (await res.json()) as Doc;
+    return launch;
 }
 
-
-export const getLatestLaunches = async () =>{
+export const getLatestLaunches = async (page = 1, pageSize = 9) => {
     const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
-    method:"POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-        query: {},
-        options: {
-            sort:{
-                date_unix: 'asc'
-            },
-            limit: 12
-        }
-    })
- })
- const { docs: launches } = (await res.json()) as APISpaceXResponse
- return launches
-}
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            query: {},
+            options: {
+                sort: {
+                    date_unix: 'asc'
+                },
+                limit: pageSize,  // Cuántos lanzamientos quieres obtener
+                page: page,   // La página que quieres obtener
+            }
+        })
+    });
 
+    const { docs: launches, totalDocs } = (await res.json()) as APISpaceXResponse;
+    return { launches, totalDocs };
+}
