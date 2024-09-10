9f27ae7c270f9bec6687327207756528e3f2694f
MiquelRoca18 <miquel1818@gmail.com>
2024-09-10 08:57:16 +0200

    Update Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
